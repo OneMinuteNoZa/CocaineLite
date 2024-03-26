aad87daf5e545c448b2a7771378a4bb5e8490a68
RedEyedLua <159163445+OneMinuteNoZa@users.noreply.github.com>
2024-03-26 13:28:12 -0500

    Update mainlite.lua
    minor save lel

--- mainlite.lua
+++ mainlite.lua
@@ -1,2 +1,13 @@
-local fluxge = getgenv()
-local exe = identifyexecutor()
+repeat task.wait() until game:IsLoaded()
+local ts = game:GetService("TextService")
+local ps = game:GetService("Players")
+local uis = game:GetService("UserInputService")
+local isfile = isfile or function(file)
+  local s , r = pcall(function() return readfile(file) end)
+  return s and r ~= nil
+
+end
+
+local setidentity = syn and syn.set_thread_identity or set_thread_identity or setidentity or setthreadidentity or function() end
+local getidentity = syn and syn.get_thread_identity or get_thread_identity or getidentity or getthreadidentity or function() return 0 end
+local LiteConfig = { -- for the config later on

9e56e62f6dcc0c1e004251065b8be9bf5219b423
RedEyedLua <159163445+OneMinuteNoZa@users.noreply.github.com>
2024-03-27 13:46:27 -0500

    Update and rename mainlite.lua to lite/mainlite.lua

---
+++ lite/mainlite.lua
@@ -0,0 +1,44 @@
+
+repeat task.wait() until game:IsLoaded()
+local getpenv = game:GetService("Players")
+local getienv = game:GetService("UserInputService")
+local pidcheck = game.PlaceId = 2753915549
+local isfile = isfile or function(file)
+  local y, n = pcall(function() return readfile(file) end)
+	return y and n ~= nil
+if not pidcheck then
+  kick("Join Blox Fruits Lel")
+end
+end)
+
+local getcustomasset = getsynasset or getcustomasset or function(location) return conf[location] or function() end)
+local customassetcheck = (getsynasset or getcustomasset) and true
+local delfile = delfile or function(file) writefile(file, "") end
+
+local setidentity = syn and syn.set_thread_identity or set_thread_identity or setidentity or setthreadidentity or function() end
+local getidentity = syn and syn.get_thread_identity or get_thread_identity or getidentity or getthreadidentity or function() return 0 end
+local configjson = isfile("lite/config.json")
+
+if InputService:GetPlatform() ~= Enum.Platform.Windows
+  getgenv().getsynasset = nil
+  getgenv().getcustomasset = nil
+  --shitty stuffs
+  getsynasset = nil
+  getcustomasset = nil
+  --ion get this shit but apparently it's needed for synasset/customasset to work
+end
+
+local function githubreq()
+if not isfile("lite/") then
+  local y, n
+  task.wait(10) function()
+    if not n then
+      print("error when connecting github, please wait...")
+    end
+end)
+
+y, n = pcall(function() return game:HttpGet("https://github.com/OneMinuteNoZa/CocaineLite/"..readfile("lite/config.json"
+        
+local function DLconfig()
+if 
+
